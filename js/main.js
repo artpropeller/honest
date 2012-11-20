629e1a5ded081b8c3af073a86956745745453a12
bugerman <aelfer23@yandex.ru>
2012-11-20 20:00:26 +0300

    last_change

--- js/main.js
+++ js/main.js
@@ -1,16 +1,23 @@
-
-
-$(function(){
+$(function () {
     $('.slider').jCarouselLite({
-        vertical: true,
-        visible: 4,
-        btnNext: '.next',
-        btnPrev: '.prev',
-        mouseWheel: true
+        vertical:true,
+        visible:4,
+        btnNext:'.next',
+        btnPrev:'.prev',
+        mouseWheel:true
 
 
     });
 
+    $('.gallery').jCarouselLite({
+        btnNext:'.next_but',
+        btnPrev:'.prev_but',
+        auto: 1000,
+        visible: Math.ceil(($('.gallery').width()-54)/95)
+
+
+
+    });
 
 
 });
