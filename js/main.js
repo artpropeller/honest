88dfcee8630498b4d5d4879841b00498b0bace07
bugerman <aelfer23@yandex.ru>
2012-11-25 15:12:47 +0300

    last_change

--- js/main.js
+++ js/main.js
@@ -12,12 +12,49 @@ $(function () {
     $('.gallery').jCarouselLite({
         btnNext:'.next_but',
         btnPrev:'.prev_but',
-        auto: 1000,
-        visible: Math.ceil(($('.gallery').width()-54)/95)
+        auto:1000,
+        visible:Math.ceil(($('.gallery').width() - 54) / 95)
 
 
 
     });
 
+    $('.consultation').click(function () {
+        $(this).parent('#container').find('.consult_window').fadeIn(500);
+
+    });
+
+    $('.svernut').click(function () {
+        $('.consult_window').fadeOut(100);
+    });
+
+    $('.close').click(function () {
+        $('.consult_window').fadeOut(100);
+    });
+
+    resizeBlock();
+    $(window).resize(function(){
+        resizeBlock();
+    });
+
 
 });
+
+function resizeBlock() {
+
+   var max = 0;
+   $('.left_part .inner, .right_part .inner').each(function () {
+       if ($(this).height() > max) {
+           max = $(this).height();
+           console.log(max);
+       }
+   });
+   $('.left_part .inner, .right_part .inner').each(function () {
+       if ($(this).height() < max) {
+           var r = max - $(this).height();
+           $(this).css('height', max+'px');
+       }
+       console.log(max);
+   });
+
+   }
